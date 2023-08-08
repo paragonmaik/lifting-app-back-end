2215534b62cd933e63bb3d1b17fe5f2fd14fbecf
Maik <estevan.maik@gmail.com>
2023-08-08 09:57:32 -0300

    chore: first commit with initial project settings

---
+++ src/main/java/com/hoister/tonshoister/TonshoisterApplication.java
@@ -0,0 +1,13 @@
+package com.hoister.tonshoister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonshoisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonshoisterApplication.class, args);
+	}
+
+}
