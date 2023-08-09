a8b473d8fc7ee5cbc9ea0df1736215bd3214ec01
Maik <estevan.maik@gmail.com>
2023-08-09 10:04:08 -0300

    feat: added query to create workouts table with dummy data

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,28 @@
+DROP TABLE IF EXISTS Exercises;
+DROP TABLE IF EXISTS Workouts;
+
 CREATE TABLE Exercises (
   id SERIAL PRIMARY KEY,
   name VARCHAR(30) NOT NULL,
   load INTEGER,
   goal VARCHAR(30) NOT NULL,
-  rest_period_in_ms INTEGER,
+  rest_period_sec INTEGER,
   description TEXT,
   date_created TIMESTAMP NOT NULL,
   date_updated TIMESTAMP
 );
 
-INSERT INTO Exercises (name, load, goal, rest_period_in_ms, description, date_created)
-VALUES ('Squat', 120, 'Strength', 150, 'High bar back squat.', CURRENT_TIMESTAMP)
+INSERT INTO Exercises (name, load, goal, rest_period_sec, description, date_created)
+VALUES ('Squat', 120, 'Strength', 150, 'High bar back squat.', CURRENT_TIMESTAMP);
+
+CREATE TABLE Workouts (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  duration_sec INTEGER,
+  description TEXT,
+  date_created TIMESTAMP NOT NULL,
+  date_updated TIMESTAMP
+);
+
+INSERT INTO Workouts (name, duration_sec, description, date_created)
+VALUES ('Workout A', 3600, 'A really cool workout.', CURRENT_TIMESTAMP);
