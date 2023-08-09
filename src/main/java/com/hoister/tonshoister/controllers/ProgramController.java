4105af6b24245434a6180b827c86f0b1f9d0f827
Maik <estevan.maik@gmail.com>
2023-08-09 15:00:11 -0300

    feat: added constructor to ProgramController

--- src/main/java/com/hoister/tonshoister/controllers/ProgramController.java
+++ src/main/java/com/hoister/tonshoister/controllers/ProgramController.java
@@ -3,8 +3,15 @@ package com.hoister.tonshoister.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hoister.tonshoister.services.ProgramService;
+
 @RestController
 @RequestMapping("/api/program")
 public class ProgramController {
+  private final ProgramService programService;
+
+  public ProgramController(ProgramService programService) {
+    this.programService = programService;
+  }
 
 }
