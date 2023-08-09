2b3c75de9b3ea665d79f7a2a6f062c071a2264e9
Maik <estevan.maik@gmail.com>
2023-08-09 16:38:32 -0300

    feat: implemented ProgramControllerAdvisor that handles ProgramNotFoundExceptions

---
+++ src/main/java/com/hoister/tonshoister/advisors/ErrorDetails.java
@@ -0,0 +1,17 @@
+package com.hoister.tonshoister.advisors;
+
+public class ErrorDetails {
+  private String message;
+
+  public ErrorDetails() {
+  }
+
+  public String getMessage() {
+    return this.message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+}
---
+++ src/main/java/com/hoister/tonshoister/advisors/ProgramNotFoundException.java
@@ -0,0 +1,5 @@
+package com.hoister.tonshoister.advisors;
+
+public class ProgramNotFoundException extends RuntimeException {
+
+}
