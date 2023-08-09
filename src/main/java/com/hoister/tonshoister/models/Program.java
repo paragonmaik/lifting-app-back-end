12ad2e5938a1f4a20b1952afc0e8012f11d945d6
Maik <estevan.maik@gmail.com>
2023-08-09 13:50:29 -0300

    feat: added constructor, getters and setters methods to the program model

--- src/main/java/com/hoister/tonshoister/models/Program.java
+++ src/main/java/com/hoister/tonshoister/models/Program.java
@@ -11,4 +11,39 @@ public class Program {
   @Column(name = "duration_weeks")
   Integer durationWeeks;
   String description;
+
+  public Program(String name, Integer durationWeeks, String description) {
+    this.name = name;
+    this.durationWeeks = durationWeeks;
+    this.description = description;
+  }
+
+  public Integer getId() {
+    return this.id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getDurationWeeks() {
+    return this.durationWeeks;
+  }
+
+  public void setDurationWeeks(Integer durationWeeks) {
+    this.durationWeeks = durationWeeks;
+  }
+
+  public String getDescription() {
+    return this.description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
 }
