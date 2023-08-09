f2643057d13d76557110b2830ab2b8e7b7d17dfb
Maik <estevan.maik@gmail.com>
2023-08-09 14:16:34 -0300

    feat: added programController

---
+++ src/main/java/com/hoister/tonshoister/controllers/ProgramController.java
@@ -0,0 +1,10 @@
+package com.hoister.tonshoister.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/program")
+public class ProgramController {
+
+}
