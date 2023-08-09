651fccfdc29ce10614ba4c5a87e9698dc08ff738
Maik <estevan.maik@gmail.com>
2023-08-09 15:00:55 -0300

    feat: mapped program class to db table

--- src/main/java/com/hoister/tonshoister/models/Program.java
+++ src/main/java/com/hoister/tonshoister/models/Program.java
@@ -1,16 +1,28 @@
 package com.hoister.tonshoister.models;
 
-import org.springframework.data.annotation.Id;
+import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 
+@Entity
+@Table
 public class Program {
   @Id
+  @SequenceGenerator(name = "program_sequence", sequenceName = "program_sequence", allocationSize = 1)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "program_sequence")
   Integer id;
   String name;
   @Column(name = "duration_weeks")
   Integer durationWeeks;
   String description;
+  @Column(name = "date_created")
+  LocalDateTime dateCreated;
 
   public Program(String name, Integer durationWeeks, String description) {
     this.name = name;
