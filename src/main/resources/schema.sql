4d6f08c531f132c119c32df019f02986651a1b8d
Maik <estevan.maik@gmail.com>
2023-08-09 10:06:12 -0300

    chore: table names are now lower case

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS Exercises;
 DROP TABLE IF EXISTS Workouts;
 
-CREATE TABLE Exercises (
+CREATE TABLE exercises (
   id SERIAL PRIMARY KEY,
   name VARCHAR(30) NOT NULL,
   load INTEGER,
@@ -15,7 +15,7 @@ CREATE TABLE Exercises (
 INSERT INTO Exercises (name, load, goal, rest_period_sec, description, date_created)
 VALUES ('Squat', 120, 'Strength', 150, 'High bar back squat.', CURRENT_TIMESTAMP);
 
-CREATE TABLE Workouts (
+CREATE TABLE workouts (
   id SERIAL PRIMARY KEY,
   name VARCHAR(30) NOT NULL,
   duration_sec INTEGER,
