bd7af3592e23a2dce1370925d936af0e7a57b072
Maik <estevan.maik@gmail.com>
2023-08-09 10:44:31 -0300

    chore: added program model to the schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,31 +1,32 @@
 DROP TABLE IF EXISTS exercise CASCADE;
 DROP TABLE IF EXISTS workout CASCADE;
 DROP TABLE IF EXISTS workout_exercise;
+DROP TABLE IF EXISTS program CASCADE;
 
 CREATE TABLE exercise (
   id SERIAL PRIMARY KEY,
   name VARCHAR(30) NOT NULL,
   load INTEGER,
   goal VARCHAR(30) NOT NULL,
-  rest_period_sec INTEGER,
+  rest_period_secs INTEGER,
   description TEXT,
   date_created TIMESTAMP NOT NULL,
   date_updated TIMESTAMP
 );
 
-INSERT INTO exercise (name, load, goal, rest_period_sec, description, date_created)
+INSERT INTO exercise (name, load, goal, rest_period_secs, description, date_created)
 VALUES ('Squat', 120, 'Strength', 150, 'High bar back squat.', CURRENT_TIMESTAMP);
 
 CREATE TABLE workout (
   id SERIAL PRIMARY KEY,
   name VARCHAR(30) NOT NULL,
-  duration_sec INTEGER,
+  duration_secs INTEGER,
   description TEXT,
   date_created TIMESTAMP NOT NULL,
   date_updated TIMESTAMP
 );
 
-INSERT INTO workout (name, duration_sec, description, date_created)
+INSERT INTO workout (name, duration_secs, description, date_created)
 VALUES ('Workout A', 3600, 'A really cool workout.', CURRENT_TIMESTAMP);
 
 CREATE TABLE workout_exercise (
@@ -33,3 +34,16 @@ CREATE TABLE workout_exercise (
   exercise_id INT REFERENCES exercise (id) ON UPDATE CASCADE,
   CONSTRAINT workout_exercise_pk PRIMARY KEY (workout_id, exercise_id)
 );
+
+CREATE TABLE program (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  duration_weeks INTEGER,
+  description TEXT,
+  date_created TIMESTAMP NOT NULL,
+  date_updated TIMESTAMP
+);
+
+INSERT INTO program (name, duration_weeks, description, date_created)
+VALUES ('Bulgarian Program', 52, 'A tought program.', CURRENT_TIMESTAMP);
+
