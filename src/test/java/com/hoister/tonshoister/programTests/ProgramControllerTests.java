95cd9cae4913229451b920a7b046b890eb1eb836
Maik <estevan.maik@gmail.com>
2023-08-10 13:21:31 -0300

    chore: moved test files to a new folder

--- src/test/java/com/hoister/tonshoister/programTests/ProgramControllerTests.java
+++ src/test/java/com/hoister/tonshoister/programTests/ProgramControllerTests.java
@@ -1,4 +1,4 @@
-package com.hoister.tonshoister;
+package com.hoister.tonshoister.programTests;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -7,14 +7,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
--- src/test/java/com/hoister/tonshoister/programTests/ProgramServiceTests.java
+++ src/test/java/com/hoister/tonshoister/programTests/ProgramServiceTests.java
@@ -1,4 +1,4 @@
-package com.hoister.tonshoister;
+package com.hoister.tonshoister.programTests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
