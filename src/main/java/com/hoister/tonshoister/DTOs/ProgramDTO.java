8627e18805c97ef5003dabcf56caf731bf166061
Maik <estevan.maik@gmail.com>
2023-08-16 14:27:36 -0300

    feat: added relation fields to programDTO and workoutDTO

--- src/main/java/com/hoister/tonshoister/DTOs/ProgramDTO.java
+++ src/main/java/com/hoister/tonshoister/DTOs/ProgramDTO.java
@@ -1,11 +1,13 @@
 package com.hoister.tonshoister.DTOs;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 public record ProgramDTO(
     Integer id,
     String name,
     Integer durationWeeks,
     String description,
-    LocalDateTime dateCreated) {
+    LocalDateTime dateCreated,
+    Set<WorkoutDTO> workouts) {
 }
--- src/main/java/com/hoister/tonshoister/DTOs/WorkoutDTO.java
+++ src/main/java/com/hoister/tonshoister/DTOs/WorkoutDTO.java
@@ -1,11 +1,13 @@
 package com.hoister.tonshoister.DTOs;
 
+import java.util.Set;
 import java.time.LocalDateTime;
 
 public record WorkoutDTO(
-        Integer id,
-        String name,
-        Integer durationMins,
-        String description,
-        LocalDateTime dateCreated) {
+    Integer id,
+    String name,
+    Integer durationMins,
+    String description,
+    LocalDateTime dateCreated,
+    Set<ProgramDTO> programs) {
 }
