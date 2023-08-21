6216a8449659dd428f4532f8af9b26982e878cb0
Maik <estevan.maik@gmail.com>
2023-08-21 15:26:57 -0300

    refactor: calls the correct service method

---
+++ src/main/java/com/hoister/tonshoister/controllers/ExerciseController.java
@@ -0,0 +1,39 @@
+package com.hoister.tonshoister.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hoister.tonshoister.DTOs.DTOsMapper;
+import com.hoister.tonshoister.DTOs.ExerciseDTO;
+import com.hoister.tonshoister.models.Exercise;
+import com.hoister.tonshoister.services.ExerciseService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/exercises")
+public class ExerciseController {
+  private final ExerciseService exerciseService;
+  private final DTOsMapper DTOsMapper;
+
+  public ExerciseController(ExerciseService exerciseService,
+      DTOsMapper DTOsMapper) {
+    this.exerciseService = exerciseService;
+    this.DTOsMapper = DTOsMapper;
+  }
+
+  @PostMapping("/{workoutId}")
+  public ResponseEntity<ExerciseDTO> createExercise(
+      @Valid @RequestBody ExerciseDTO exerciseDTO, @PathVariable Integer workoutId) {
+    Exercise createdExercise = exerciseService
+        .createExercise(DTOsMapper.convertToEntity(exerciseDTO), workoutId);
+
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(DTOsMapper.convertToDto(createdExercise));
+  }
+}
