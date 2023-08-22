66d8d1cdeb3ccd4583639ad990edf7501cbc22a1
Maik <estevan.maik@gmail.com>
2023-08-22 14:13:41 -0300

    refactor: removed unnecessary import

--- src/test/java/com/hoister/tonshoister/programTests/ExerciseServiceTests.java
+++ src/test/java/com/hoister/tonshoister/programTests/ExerciseServiceTests.java
@@ -11,7 +11,6 @@ import static org.mockito.Mockito.when;
 
 import java.util.Optional;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
