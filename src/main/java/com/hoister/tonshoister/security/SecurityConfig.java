574d5b46f7187742815fcb2f8c49c96866d9dbcd
Maik <estevan.maik@gmail.com>
2023-08-26 10:30:51 -0300

    feat: added authenticationManager method

--- src/main/java/com/hoister/tonshoister/security/SecurityConfig.java
+++ src/main/java/com/hoister/tonshoister/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.hoister.tonshoister.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,5 +22,11 @@ public class SecurityConfig {
             .requestMatchers("/auth/*").permitAll()
             .anyRequest().authenticated())
         .build();
+  }
+
+  @Bean
+  AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
   }
 }
