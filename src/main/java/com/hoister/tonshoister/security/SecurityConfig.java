7507a5acafb6f68fbc982c30bf5cc577548f0641
Maik <estevan.maik@gmail.com>
2023-08-26 09:24:38 -0300

    feat: implemented SecurityConfig class

---
+++ src/main/java/com/hoister/tonshoister/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.hoister.tonshoister.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(authz -> authz
+            .requestMatchers("/api/auth/*").permitAll()
+            .anyRequest().authenticated())
+        .build();
+  }
+}
