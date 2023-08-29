b23a7fb6a88c1889baeddbce229efb8e0ae132a7
Maik <estevan.maik@gmail.com>
2023-08-29 14:24:21 -0300

    feat: implemented register and login user controller methods and its tests

---
+++ src/main/java/com/hoister/tonshoister/controllers/AuthenticationController.java
@@ -0,0 +1,41 @@
+package com.hoister.tonshoister.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hoister.tonshoister.DTOs.AuthenticationDTO;
+import com.hoister.tonshoister.DTOs.DTOsMapper;
+import com.hoister.tonshoister.DTOs.LoginResponseDTO;
+import com.hoister.tonshoister.DTOs.RegisterDTO;
+import com.hoister.tonshoister.services.AuthenticationService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+
+  @Autowired
+  private AuthenticationService authenticationService;
+  @Autowired
+  private DTOsMapper DTOsMapper;
+
+  @PostMapping("/register")
+  public ResponseEntity<Void> register(@RequestBody @Valid RegisterDTO data) {
+    authenticationService.registerUser(
+        DTOsMapper.convertToEntity(data));
+
+    return ResponseEntity.ok().build();
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid AuthenticationDTO data) {
+    String token = authenticationService.loginUser(data);
+
+    return ResponseEntity.ok(new LoginResponseDTO(token));
+  }
+}
---
+++ src/test/java/com/hoister/tonshoister/authentication/AuthenticationControllerTests.java
@@ -0,0 +1,81 @@
+package com.hoister.tonshoister.authentication;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hoister.tonshoister.DTOs.AuthenticationDTO;
+import com.hoister.tonshoister.DTOs.DTOsMapper;
+import com.hoister.tonshoister.DTOs.RegisterDTO;
+import com.hoister.tonshoister.controllers.AuthenticationController;
+import com.hoister.tonshoister.models.User;
+import com.hoister.tonshoister.models.UserRole;
+import com.hoister.tonshoister.repositories.UserRepository;
+import com.hoister.tonshoister.security.TokenService;
+import com.hoister.tonshoister.services.AuthenticationService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(AuthenticationController.class)
+public class AuthenticationControllerTests {
+
+  @Autowired
+  ObjectMapper objectMapper;
+
+  @MockBean
+  UserRepository userRepository;
+  @MockBean
+  TokenService tokenService;
+  @MockBean
+  AuthenticationService authenticationService;
+  @MockBean
+  DTOsMapper DTOsMapper;
+  @Autowired
+  MockMvc mockMvc;
+
+  @Test
+  public void registerUserSuccess() throws Exception {
+    User user = new User(
+        "arnold", "gettothechoppah", UserRole.ADMIN);
+
+    when(DTOsMapper.convertToEntity(any(RegisterDTO.class))).thenReturn(user);
+    doNothing().when(authenticationService).registerUser(user);
+
+    mockMvc
+        .perform(
+            post("/api/auth/register").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(user)))
+        .andExpect(MockMvcResultMatchers.status().isOk());
+
+    verify(authenticationService).registerUser(user);
+  }
+
+  @Test
+  public void loginUserSuccess() throws Exception {
+    AuthenticationDTO authDTO = new AuthenticationDTO(
+        "arnold", "gettothechoppah");
+
+    when(authenticationService.loginUser(authDTO)).thenReturn("");
+
+    mockMvc
+        .perform(
+            post("/api/auth/login").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(authDTO)))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.token").exists())
+        .andExpect(MockMvcResultMatchers.status().isOk());
+
+    verify(authenticationService).loginUser(authDTO);
+  }
+}
