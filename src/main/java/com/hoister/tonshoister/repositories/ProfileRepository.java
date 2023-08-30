d63cc616686f3805520867eb7fd7fcf112f7060c
Maik <estevan.maik@gmail.com>
2023-08-30 09:46:56 -0300

    feat: created ProfileRepository

---
+++ src/main/java/com/hoister/tonshoister/repositories/ProfileRepository.java
@@ -0,0 +1,8 @@
+package com.hoister.tonshoister.repositories;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+import com.hoister.tonshoister.models.Profile;
+
+public interface ProfileRepository extends ListCrudRepository<Profile, Integer> {
+}
