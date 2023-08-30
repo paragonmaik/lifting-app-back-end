1ca20099a078dc3ae8aea5e803677620b36ede15
Maik <estevan.maik@gmail.com>
2023-08-30 13:45:05 -0300

    feat: added updateProfile method and its tests to the service layer

--- src/main/java/com/hoister/tonshoister/services/ProfileService.java
+++ src/main/java/com/hoister/tonshoister/services/ProfileService.java
@@ -3,6 +3,7 @@ package com.hoister.tonshoister.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.hoister.tonshoister.advisors.ProfileNotFoundException;
 import com.hoister.tonshoister.models.Profile;
 import com.hoister.tonshoister.models.User;
 import com.hoister.tonshoister.repositories.ProfileRepository;
@@ -18,5 +19,15 @@ public class ProfileService {
     profile.setUser(user);
 
     profileRepository.save(profile);
+  }
+
+  public Profile updateProfile(Profile profile) throws Exception {
+    Profile foundProfile = profileRepository.findById(profile.getId())
+        .orElseThrow(() -> new ProfileNotFoundException());
+
+    foundProfile.setWeight(profile.getWeight());
+    foundProfile.setHeight(profile.getHeight());
+
+    return profileRepository.save(foundProfile);
   }
 }
--- src/test/java/com/hoister/tonshoister/profile/ProfileServiceTests.java
+++ src/test/java/com/hoister/tonshoister/profile/ProfileServiceTests.java
@@ -1,8 +1,12 @@
 package com.hoister.tonshoister.profile;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.hoister.tonshoister.advisors.ProfileNotFoundException;
 import com.hoister.tonshoister.models.Profile;
 import com.hoister.tonshoister.models.User;
 import com.hoister.tonshoister.models.UserRole;
@@ -33,5 +38,35 @@ public class ProfileServiceTests {
     profileService.createProfile(user);
 
     verify(profileRepository).save(any(Profile.class));
+  }
+
+  @Test
+  public void updateProfileSuccess() throws Exception {
+    Profile profile1 = new Profile("uuid", 70, 175, null);
+    Profile profile2 = new Profile("uuid", 75, 176, null);
+
+    when(profileRepository.findById("uuid")).thenReturn(Optional.of(profile1));
+    when(profileRepository.save(profile1)).thenReturn(profile2);
+
+    Profile updatedProfile = profileService.updateProfile(profile1);
+
+    assertNotEquals(profile1.getWeight(), updatedProfile.getWeight());
+    assertNotEquals(profile1.getHeight(), updatedProfile.getHeight());
+
+    verify(profileRepository).findById("uuid");
+    verify(profileRepository).save(profile1);
+  }
+
+  @Test
+  public void updateProfileThrowsException() {
+    Profile profile1 = new Profile("uuid", 70, 175, null);
+
+    when(profileRepository.findById("uuid")).thenReturn(Optional.empty());
+
+    assertThrows(ProfileNotFoundException.class, () -> {
+      profileService.updateProfile(profile1);
+    });
+
+    verify(profileRepository).findById("uuid");
   }
 }
