6ac82031c9282b575dbb050740d0e2a8646d04bd
Maik <estevan.maik@gmail.com>
2023-09-01 18:09:29 -0300

    feat: declared the findByUserId method in the ProgramRepository interface

--- src/main/java/com/hoister/tonshoister/repositories/ProgramRepository.java
+++ src/main/java/com/hoister/tonshoister/repositories/ProgramRepository.java
@@ -3,6 +3,8 @@ package com.hoister.tonshoister.repositories;
 import org.springframework.data.repository.ListCrudRepository;
 
 import com.hoister.tonshoister.models.Program;
+import java.util.List;
 
 public interface ProgramRepository extends ListCrudRepository<Program, Integer> {
+  List<Program> findByUserId(String userId);
 }
