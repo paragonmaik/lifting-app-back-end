560cdd0fe5ae55d987e1a8943d97e3c3df2a64a2
Maik <estevan.maik@gmail.com>
2023-09-03 13:29:13 -0300

    feat: added findAllByUserId method to the WorkoutRepository

--- src/main/java/com/hoister/tonshoister/repositories/WorkoutRepository.java
+++ src/main/java/com/hoister/tonshoister/repositories/WorkoutRepository.java
@@ -1,8 +1,11 @@
 package com.hoister.tonshoister.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.ListCrudRepository;
 
 import com.hoister.tonshoister.models.Workout;
 
 public interface WorkoutRepository extends ListCrudRepository<Workout, Integer> {
+  List<Workout> findAllByUserId(String userId);
 }
