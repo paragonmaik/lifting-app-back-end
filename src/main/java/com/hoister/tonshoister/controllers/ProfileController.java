99322590eaeb68eecc2a5119a41d677a9422c536
Maik <estevan.maik@gmail.com>
2023-09-04 13:48:13 -0300

    refactor: added wildcard imports to ProfileController

--- src/main/java/com/hoister/tonshoister/controllers/ProfileController.java
+++ src/main/java/com/hoister/tonshoister/controllers/ProfileController.java
@@ -2,11 +2,7 @@ package com.hoister.tonshoister.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.hoister.tonshoister.DTOs.DTOsMapper;
 import com.hoister.tonshoister.DTOs.ProfileDTO;
