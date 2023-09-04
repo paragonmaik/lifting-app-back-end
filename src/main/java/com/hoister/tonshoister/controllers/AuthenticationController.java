160c626c9bb5a25116cabcf9c681604194b818c4
Maik <estevan.maik@gmail.com>
2023-09-04 13:42:33 -0300

    refactor: added wildcard imports

--- src/main/java/com/hoister/tonshoister/controllers/AuthenticationController.java
+++ src/main/java/com/hoister/tonshoister/controllers/AuthenticationController.java
@@ -2,10 +2,7 @@ package com.hoister.tonshoister.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.hoister.tonshoister.DTOs.AuthenticationDTO;
 import com.hoister.tonshoister.DTOs.DTOsMapper;
