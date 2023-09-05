65663fe9be8a9a0b8566022301f2263ef3122f8e
Maik <estevan.maik@gmail.com>
2023-09-05 17:23:39 -0300

    feat: updated the schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,82 +1,75 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS profile CASCADE;
 DROP TABLE IF EXISTS exercise CASCADE;
+DROP SEQUENCE IF EXISTS exercise_sequence;
 DROP TABLE IF EXISTS workout CASCADE;
+DROP SEQUENCE IF EXISTS workout_sequence;
 DROP TABLE IF EXISTS workout_exercise;
 DROP TABLE IF EXISTS program CASCADE;
 DROP SEQUENCE IF EXISTS program_sequence;
 DROP TABLE IF EXISTS program_workout;
-DROP TABLE IF EXISTS profile CASCADE;
+
+
+CREATE TABLE users (
+  id TEXT PRIMARY KEY UNIQUE NOT NULL,
+  login TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL,
+  role TEXT NOT NULL
+);
+
+CREATE TABLE profile (
+  user_id TEXT PRIMARY KEY REFERENCES users (id),
+  weight INTEGER,
+  height INTEGER,
+  date_created TIMESTAMP
+);
 
 CREATE TABLE exercise (
   id SERIAL PRIMARY KEY,
+  user_id TEXT REFERENCES profile (user_id),
   name VARCHAR(30) NOT NULL,
   load INTEGER,
   goal VARCHAR(30) NOT NULL,
-  rest_period_secs INTEGER,
-  description TEXT,
+  rest_seconds INTEGER,
+  instructions TEXT,
   date_created TIMESTAMP NOT NULL,
   date_updated TIMESTAMP
 );
 
-INSERT INTO exercise (name, load, goal, rest_period_secs, description, date_created)
-VALUES ('Squat', 120, 'Strength', 150, 'High bar back squat.', CURRENT_TIMESTAMP);
+CREATE SEQUENCE exercise_sequence START WITH 1 INCREMENT BY 1 MINVALUE 1;
 
 CREATE TABLE workout (
   id SERIAL PRIMARY KEY,
+  user_id TEXT REFERENCES profile (user_id),
   name VARCHAR(30) NOT NULL,
-  duration_secs INTEGER,
+  duration_mins INTEGER,
   description TEXT,
   date_created TIMESTAMP NOT NULL,
   date_updated TIMESTAMP
 );
 
-INSERT INTO workout (name, duration_secs, description, date_created)
-VALUES ('Workout A', 3600, 'A really cool workout.', CURRENT_TIMESTAMP);
+CREATE SEQUENCE workout_sequence START WITH 1 INCREMENT BY 1 MINVALUE 1;
 
 CREATE TABLE workout_exercise (
-  workout_id INT REFERENCES workout (id) ON UPDATE CASCADE,
-  exercise_id INT REFERENCES exercise (id) ON UPDATE CASCADE,
+  workout_id INT REFERENCES workout (id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+  exercise_id INT REFERENCES exercise (id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
   CONSTRAINT workout_exercise_pk PRIMARY KEY (workout_id, exercise_id)
-);
-
-INSERT INTO workout_exercise (workout_id, exercise_id)
-VALUES (
-  (SELECT id FROM workout WHERE name = 'Workout A'),
-  (SELECT id FROM exercise WHERE name = 'Squat')
 );
 
 CREATE TABLE program (
   id SERIAL PRIMARY KEY,
+  user_id TEXT REFERENCES profile (user_id),
   name VARCHAR(30) NOT NULL,
   duration_weeks INTEGER,
   description TEXT,
-  date_created TIMESTAMP, --NOT NULL,
+  date_created TIMESTAMP,
   date_updated TIMESTAMP
 );
-
-INSERT INTO program (name, duration_weeks, description, date_created)
-VALUES ('Bulgarian Program', 52, 'A tough program.', CURRENT_TIMESTAMP);
 
 CREATE SEQUENCE program_sequence START WITH 1 INCREMENT BY 1 MINVALUE 1;
 
 CREATE TABLE program_workout (
-  program_id INT REFERENCES program (id) ON UPDATE CASCADE ON DELETE CASCADE,
-  workout_id INT REFERENCES workout (id) ON UPDATE CASCADE ON DELETE CASCADE,
+  program_id INT REFERENCES program (id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+  workout_id INT REFERENCES workout (id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
   CONSTRAINT program_workout_pk PRIMARY KEY (program_id, workout_id)
 );
-
-INSERT INTO program_workout (program_id, workout_id)
-VALUES (
-  (SELECT id FROM program WHERE name = 'Bulgarian Program'),
-  (SELECT id FROM workout WHERE name = 'Workout A')
-);
-
-CREATE TABLE profile (
-  id SERIAL PRIMARY KEY,
-  weight INTEGER,
-  height INTEGER,
-  date_created TIMESTAMP NOT NULL,
-  date_updated TIMESTAMP
-);
-
-INSERT INTO profile (weight, height, date_created)
-VALUES (97, 190, CURRENT_TIMESTAMP);
