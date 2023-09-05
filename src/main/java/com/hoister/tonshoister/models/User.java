e012e0f32ac733b20994dd23a6123d0087103313
Maik <estevan.maik@gmail.com>
2023-09-05 08:06:29 -0300

    refactor: added wildcard imports

--- src/main/java/com/hoister/tonshoister/models/User.java
+++ src/main/java/com/hoister/tonshoister/models/User.java
@@ -7,15 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.PrimaryKeyJoinColumn;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
