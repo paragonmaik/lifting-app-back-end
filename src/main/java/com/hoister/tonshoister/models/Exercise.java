a98442c9ba7b6fd3b85f09c642bcd91fa723e6e4
maik <estevan.maik@gmail.com>
2023-10-20 09:45:20 -0300

    refactor: added lombok annotations to the Exercise model

--- src/main/java/com/hoister/tonshoister/models/Exercise.java
+++ src/main/java/com/hoister/tonshoister/models/Exercise.java
@@ -8,9 +8,15 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Exercise {
   @Id
   @SequenceGenerator(name = "exercise_sequence", sequenceName = "exercise_sequence", allocationSize = 1)
@@ -37,121 +43,4 @@ public class Exercise {
 
   @ManyToMany(mappedBy = "exercises", fetch = FetchType.LAZY)
   private Set<Workout> workouts = new HashSet<>();
-
-  public Exercise() {
-  }
-
-  public Exercise(Integer id, String userId, String name, Integer load, GoalType goal, Integer restSeconds,
-      String instructions, LocalDateTime dateCreated, Integer sets, Integer reps, Integer execOrder,
-      Set<Workout> workouts) {
-    this.id = id;
-    this.userId = userId;
-    this.name = name;
-    this.load = load;
-    this.goal = goal;
-    this.restSeconds = restSeconds;
-    this.instructions = instructions;
-    this.dateCreated = dateCreated;
-    this.sets = sets;
-    this.reps = reps;
-    this.execOrder = execOrder;
-    this.workouts = workouts;
-  }
-
-  public String getUserId() {
-    return this.userId;
-  }
-
-  public void setUserId(String userId) {
-    this.userId = userId;
-  }
-
-  public Integer getId() {
-    return this.id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return this.name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public Integer getLoad() {
-    return this.load;
-  }
-
-  public void setLoad(Integer load) {
-    this.load = load;
-  }
-
-  public GoalType getGoal() {
-    return this.goal;
-  }
-
-  public void setGoal(GoalType goal) {
-    this.goal = goal;
-  }
-
-  public Integer getRestSeconds() {
-    return this.restSeconds;
-  }
-
-  public void setRestSeconds(Integer restSeconds) {
-    this.restSeconds = restSeconds;
-  }
-
-  public String getInstructions() {
-    return this.instructions;
-  }
-
-  public void setInstructions(String instructions) {
-    this.instructions = instructions;
-  }
-
-  public LocalDateTime getDateCreated() {
-    return this.dateCreated;
-  }
-
-  public void setDateCreated(LocalDateTime dateCreated) {
-    this.dateCreated = dateCreated;
-  }
-
-  public Set<Workout> getWorkouts() {
-    return this.workouts;
-  }
-
-  public void setWorkouts(Set<Workout> workouts) {
-    this.workouts = workouts;
-  }
-
-  public Integer getSets() {
-    return this.sets;
-  }
-
-  public void setSets(Integer sets) {
-    this.sets = sets;
-  }
-
-  public Integer getReps() {
-    return this.reps;
-  }
-
-  public void setReps(Integer reps) {
-    this.reps = reps;
-  }
-
-  public Integer getExecOrder() {
-    return this.execOrder;
-  }
-
-  public void setExecOrder(Integer execOrder) {
-    this.execOrder = execOrder;
-  }
-
 }
