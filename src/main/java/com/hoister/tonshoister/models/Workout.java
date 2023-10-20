573301ffd7fb946d7c50c78a5d643f105d36c7d3
maik <estevan.maik@gmail.com>
2023-10-20 10:03:24 -0300

    refactor: added lombok annotations to the Workout model

--- src/main/java/com/hoister/tonshoister/models/Workout.java
+++ src/main/java/com/hoister/tonshoister/models/Workout.java
@@ -8,9 +8,17 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Workout {
   @Id
   @SequenceGenerator(name = "workout_sequence", sequenceName = "workout_sequence", allocationSize = 1)
@@ -37,82 +45,4 @@ public class Workout {
           @JoinColumn(name = "exercise_id") })
   private Set<Exercise> exercises = new HashSet<>();
 
-  public Workout() {
-  }
-
-  public Workout(Integer id, String userId, String name, Integer durationMins, String description,
-      LocalDateTime dateCreated, Set<Program> programs, Set<Exercise> exercises) {
-    this.id = id;
-    this.userId = userId;
-    this.name = name;
-    this.durationMins = durationMins;
-    this.description = description;
-    this.dateCreated = dateCreated;
-    this.programs = programs;
-    this.exercises = exercises;
-  }
-
-  public String getUserId() {
-    return this.userId;
-  }
-
-  public void setUserId(String userId) {
-    this.userId = userId;
-  }
-
-  public Set<Exercise> getExercises() {
-    return this.exercises;
-  }
-
-  public void setExercises(Set<Exercise> exercises) {
-    this.exercises = exercises;
-  }
-
-  public Integer getId() {
-    return this.id;
-  }
-
-  public Set<Program> getPrograms() {
-    return this.programs;
-  }
-
-  public void setPrograms(Set<Program> programs) {
-    this.programs = programs;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return this.name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public Integer getDurationMins() {
-    return this.durationMins;
-  }
-
-  public void setDurationMins(Integer durationMins) {
-    this.durationMins = durationMins;
-  }
-
-  public String getDescription() {
-    return this.description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public LocalDateTime getDateCreated() {
-    return this.dateCreated;
-  }
-
-  public void setDateCreated(LocalDateTime dateCreated) {
-    this.dateCreated = dateCreated;
-  }
 }
