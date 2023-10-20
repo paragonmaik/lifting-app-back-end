537a8f4463226f40c4b3bd67c7ae434954f4e42c
maik <estevan.maik@gmail.com>
2023-10-20 09:49:03 -0300

    refactor: added lombok annotations to the Program model

--- src/main/java/com/hoister/tonshoister/models/Program.java
+++ src/main/java/com/hoister/tonshoister/models/Program.java
@@ -8,9 +8,15 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Program {
   @Id
   @SequenceGenerator(name = "program_sequence", sequenceName = "program_sequence", allocationSize = 1)
@@ -33,74 +39,4 @@ public class Program {
       @JoinColumn(name = "program_id") }, inverseJoinColumns = {
           @JoinColumn(name = "workout_id") })
   private Set<Workout> workouts = new HashSet<>();
-
-  public Program() {
-  }
-
-  public Program(Integer id, String userId, String name, Integer durationWeeks, String description,
-      LocalDateTime dateCreated, Set<Workout> workouts) {
-    this.id = id;
-    this.userId = userId;
-    this.name = name;
-    this.durationWeeks = durationWeeks;
-    this.description = description;
-    this.dateCreated = dateCreated;
-    this.workouts = workouts;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getUserId() {
-    return this.userId;
-  }
-
-  public void setUserId(String userId) {
-    this.userId = userId;
-  }
-
-  public Set<Workout> getWorkouts() {
-    return this.workouts;
-  }
-
-  public void setWorkouts(Set<Workout> workouts) {
-    this.workouts = workouts;
-  }
-
-  public Integer getId() {
-    return this.id;
-  }
-
-  public String getName() {
-    return this.name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public Integer getDurationWeeks() {
-    return this.durationWeeks;
-  }
-
-  public void setDurationWeeks(Integer durationWeeks) {
-    this.durationWeeks = durationWeeks;
-  }
-
-  public String getDescription() {
-    return this.description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public LocalDateTime getDateCreated() {
-    return this.dateCreated;
-  }
-
-  public void setDateCreated(LocalDateTime dateCreated) {
-    this.dateCreated = dateCreated;
-  }
 }
